293f36dcb59eba7b936eb05ff53c1f7aa2700395
linhtpm-pvn <linhtpm@pvn.vn>
2025-10-06 02:26:40 +0000

    feat: update on main before merging into dev

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { Button} from './components/ui/Button';
-import { Input } from './components/ui/Input';
-import { Label } from './components/ui/Label';
-import { Select } from './components/ui/Select';
-import { Textarea } from './components/ui/Textarea';
+import { Button} from './modules/module1/react-components/Button';
+import { Input } from './modules/module2/react-components/Input';
+import { Label } from '././modules/module2/react-components/Label';
+import { Select } from '././modules/module3/react-components/Select';
+import { Textarea } from '././modules/module3/react-components/Textarea';
 export {
     Button,
     Input,
